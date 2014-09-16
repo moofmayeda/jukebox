79243c68e2aed37ae2ab00f1a8a7305b7e51d3b3
moof mayeda <moofmayeda@yahoo.com>
2014-09-16 10:48:54 -0700

    allow admin to delete a track from the playlist

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,11 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
 
-  before_create :search_song
+  before_create :add_song
+  before_destroy :remove_song
 
 private
-  def search_song
+  def authenticate_client
     @client = Soundcloud.new(:client_id => ENV['Client_ID'],
                         :client_secret => ENV['Secret_Client_ID'],
                         :username => ENV['username'],
@@ -12,11 +13,26 @@ private
                         :grant_type => 'password',
                         :content_type => 'application/x-www-form-urlencoded',
                         :scope => 'non-expiring')
+  end
+
+  def add_song
+    authenticate_client
     playlist = @client.get("/me/playlists").first
     track_ids = playlist.tracks.map(&:id)
     track_id = @client.get('/tracks', :q => self.title + " " + self.artist)[0]["id"]
     track_ids << track_id
     self.track_id = track_id
+    tracks = track_ids.map{|id| {:id => id}}
+    playlist = @client.put(playlist.uri, :playlist => { :tracks => tracks })
+    playlist.tracks.map(&:id)
+  end
+
+  def remove_song
+    authenticate_client
+    playlist = @client.get("/me/playlists").first
+    track_ids = playlist.tracks.map(&:id)
+    track_id = @client.get('/tracks', :q => self.title + " " + self.artist)[0]["id"]
+    track_ids.delete(self.track_id)
     tracks = track_ids.map{|id| {:id => id}}
     playlist = @client.put(playlist.uri, :playlist => { :tracks => tracks })
     playlist.tracks.map(&:id)
