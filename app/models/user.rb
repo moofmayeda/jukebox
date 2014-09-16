dc31b06380e5a6f1f65e30b79861ddca1a8f69d4
moof mayeda <moofmayeda@yahoo.com>
2014-09-16 13:10:29 -0700

    generate a token for each new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :generate_token
+
+private
+  def generate_token
+    begin
+      self.token = SecureRandom.hex
+    end while User.exists?(token: token)
+  end
 end
